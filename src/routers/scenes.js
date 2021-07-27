4dc0ce2c8d34f42899511a1cf07f2ad73db267c1
amda <amda.prog@gmail.com>
2021-07-27 11:17:45 +0200

    sortBy closest_to asc working

--- src/routers/scenes.js
+++ src/routers/scenes.js
@@ -1,19 +1,35 @@
 const { Router } = require("express");
 
+const MyMovieTripDB = require("@DB");
 const Scenes = require("Models/scenes");
 
 const router = new Router();
 
 router.get("/scenes", async (req, res) => {
-  let parts = [];
-  if (req.query.creation_date_sort) {
-    parts.push(["creation_date", req.query.creation_date_sort]);
-  }
-
   try {
-    const scenes = await Scenes.findAll({
-      order: [parts],
-    });
+    let scenes;
+    if (req.query.sortBy) {
+      let parts = [];
+      if (req.query.sortBy.includes("creation_date")) {
+        parts.push([
+          "creation_date",
+          req.query.sortBy.includes("desc") ? "DESC" : "ASC",
+        ]);
+        scenes = await Scenes.findAll({
+          order: [parts],
+        });
+      } else if (req.query.sortBy.includes("closest_to")) {
+        let metadata;
+        const coords = req.query.sortBy.split("_")[2];
+        const user_lat = coords.split(",")[0];
+        const user_lng = coords.split(",")[1];
+        const query = `SELECT *, SUBSTRING_INDEX(location, ', ', 1) AS lat, SUBSTRING_INDEX(location, ', ', -1) AS lng FROM scenes ORDER BY ((lat-${user_lat})*(lat-${user_lat})) + ((lng - ${user_lng})*(lng - ${user_lng})) ASC`;
+        [scenes, metadata] = await MyMovieTripDB.query(query);
+      }
+    } else {
+      scenes = await Scenes.findAll();
+    }
+
     return res.send(scenes);
   } catch (error) {
     console.log(error);
